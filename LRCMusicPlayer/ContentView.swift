325c99004e5a5a504ee8775e5f2f1d458fccdf74
LIJING <lj430878@antgroup.com>
2024-03-22 16:45:39 +0800

    Initial Commit

---
+++ LRCMusicPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LRCMusicPlayer
+//
+//  Created by 李京 on 2024/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LRCMusicPlayer/LRCMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LRCMusicPlayerApp.swift
+//  LRCMusicPlayer
+//
+//  Created by 李京 on 2024/3/22.
+//
+
+import SwiftUI
+
+@main
+struct LRCMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
